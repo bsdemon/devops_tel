ed7d71d4806780e4d9ce623768f323d1a95ed4e9
Ilia Iliev <bsdemon@gmail.com>
2025-03-07 01:36:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,5 +13,13 @@ rm argocd-linux-amd64
 kubectl get pod -n argocd
 kubectl get svc -n argocd
 
+# get generated password
 # kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode && echo
+
+# forward web-server port and proceed without https
+# kubectl port-forward svc/argocd-server -n argocd 8080:443 
+
+# This will make local ssh port forwarding. Open at http://localhost:8080 
 # ssh -fNT -L 8080:localhost:8080 root@<ip_address>
+
+# If you want to expose argocd web server, create ingress with `traefik`
